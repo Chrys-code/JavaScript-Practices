d655b81e5a3085ac05f5199bb198de35d8b54c71
Krisztian Nagy <k.nagy.k98@gmail.com>
2020-07-20 11:42:16 +0200

    validator moved to module

--- 11-Course-Form/js/app.js
+++ 11-Course-Form/js/app.js
@@ -1,7 +1,7 @@
+import { validator, inputs } from "./validatormodule.js";
+
 //Variables
-const inputs = document.querySelectorAll("input"); // customerName, Course, Author
 const form = document.querySelector("form");
-const error = document.querySelectorAll(".input-group-text");
 //EventListeners
 document.addEventListener("DOMContentLoaded", validator);
 form.addEventListener("submit", addCourse);
@@ -28,36 +28,6 @@ function addCourse(e) {
   for (var i = 0; i < inputs.length; i++) {
     inputs[i].value = "";
   }
-}
-
-//Validator (feedback)
-function validator() {
-  inputs.forEach((input) => {
-    input.addEventListener("keyup", () => {
-      //Name
-      if (input.classList.contains("name")) {
-        if (inputs[0].value === null || inputs[0].value === "") {
-          error[0].innerHTML = "Please enter Customer Name";
-        } else {
-          error[0].innerHTML = "<i class='fas fa-user'></i>";
-        }
-      }
-      //Course
-      if (input.classList.contains("course"))
-        if (inputs[1].value === null || inputs[1].value === "") {
-          error[1].innerHTML = "Please enter Course Name";
-        } else {
-          error[1].innerHTML = "<i class='fas fa-book'></i>";
-        }
-      //Author
-      if (input.classList.contains("author"))
-        if (inputs[2].value === null || inputs[2].value === "") {
-          error[2].innerHTML = "Please enter Author Name";
-        } else {
-          error[2].innerHTML = "<i class='fas fa-user-circle'></i>";
-        }
-    });
-  });
 }
 
 function Display() {
